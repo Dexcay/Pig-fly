dbcf2ef5ba941419916908bdae3d306ea5a56c4d
Dexcay <kinobiysk@gmail.com>
2025-12-13 14:48:54 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1765611897,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/pig-sheet0.png",
+		"images/meteor-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/life-sheet0.png",
+		"images/hp1-sheet0.png",
+		"images/Спрайт-sheet0.png",
+		"images/bit-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
